65da44f20b9eda6764960dea8b49a3cc2308a5a0
msk84 <florian@steinle.it>
2023-12-01 22:30:36 +0100

    * Day1, Part2

--- src/main/java/net/msk/adventOfCode/Day01_Trebuchet.java
+++ src/main/java/net/msk/adventOfCode/Day01_Trebuchet.java
@@ -1,11 +1,31 @@
 package net.msk.adventOfCode;
 
 import java.util.List;
+import java.util.Map;
 
 public class Day01_Trebuchet {
 
     public static int calculateCalibrationValueSum(final List<String> input) {
+
+        final Map<String, String> replaceMap = Map.of(
+                "one", "o1e",
+                "two", "t2o",
+                "three", "t3e",
+                "four", "f4r",
+                "five", "f5e",
+                "six", "s6x",
+                "seven", "s7n",
+                "eight", "e8t",
+                "nine", "n9e"
+        );
+
         return input.stream()
+                .map(s -> {
+                    for(var entry : replaceMap.entrySet()) {
+                        s = s.replace(entry.getKey(), entry.getKey() + entry.getValue());
+                    }
+                    return s;
+                })
                 .map(s -> s.replaceAll("[a-zA-Z]", ""))
                 .map(s -> s.charAt(0) + s.substring(s.length() - 1))
                 .mapToInt(Integer::parseInt)
--- src/test/java/net/msk/adventOfCode/Day01_TrebuchetTest.java
+++ src/test/java/net/msk/adventOfCode/Day01_TrebuchetTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day01TrebuchetTest extends BaseTest {
 
     @Test
-    void task1Test_Try() {
+    void task1TestTry() {
         final List<String> input = Stream.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet").toList();
         final int result = Day01_Trebuchet.calculateCalibrationValueSum(input);
         assertEquals(142, result);
@@ -22,6 +22,13 @@ class Day01TrebuchetTest extends BaseTest {
     void task1Test() throws IOException, URISyntaxException {
         final List<String> data = this.getDataFromFileAsList("day01_data");
         final int result = Day01_Trebuchet.calculateCalibrationValueSum(data);
-        assertEquals(55017, result);
+        assertEquals(53539, result);
+    }
+
+    @Test
+    void task2Try() {
+        final List<String> input = Stream.of("two1nine","eightwothree","abcone2threexyz","xtwone3four","4nineeightseven2","zoneight234","7pqrstsixteen").toList();
+        final int result = Day01_Trebuchet.calculateCalibrationValueSum(input);
+        assertEquals(281, result);
     }
 }
